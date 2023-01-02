ada76f038ff7006868ebeaa81d1b52e334d798fa
mart_laptop <mart.veldkamp@gmail.com>
2023-01-02 22:40:32 +0100

    made another object class. It stores object data

--- Conventional.py
+++ Conventional.py
@@ -9,23 +9,28 @@ import glob
 class image():
     
     def __init__(self, cv_image, name):
-        self.contours = []
-        self.cv_image = cv_image
-        self.name = name
-    
-    def set_color(self, color):
-        self.color = color
+        self.contours   = []
+        self.cv_image   = cv_image
+        self.name       = name
     
     def add_contour(self, contour):
         self.contours.append(contour)
-            
+
+# TODO: Deze nog toepassen in code
+class object():
+    def __init__(self):
+        self.kind_of_object = None
+        self.color          = None
+        self.position       = None
+        self.oriëntation    = None
+    
 # -=-=-=- VARIABLES -=-=-=- #
 
 # Folder variables
 images      = []
 root_dir    = os.path.abspath("./")
 input_dir   = os.path.join(root_dir,'input_conventional','*.jpg')
-output_dir  = None # Wordt later gedefinieerd 
+output_dir  = None # later defined 
 
 #                      Hue, Saturation, Value
 lower_color = np.array([10,     20,      35])
@@ -38,15 +43,15 @@ kernel = np.ones((4, 4), np.uint8)
 # -=-=-=- READ IMAGES  -=-=-=- #
 
 for img in glob.glob(input_dir):
-    cv_img = cv.imread(img)
-    img = image(cv_img, os.path.basename(img))
+    cv_img  = cv.imread(img)
+    img     = image(cv_img, os.path.basename(img))
     images.append(img)
 
 # -=-=-=- MASK IMAGES -=-=-=- #
 
 for img in images:
-
-    # set afbeelding in hue color space
+    
+    # set image in hue color space
     hsv_image = cv.cvtColor(img.cv_image, cv.COLOR_BGR2HSV)
     
     # erode / dilate images for better quality masks
@@ -77,13 +82,16 @@ for img in images:
     # om een specifieke contour te maken, gebruik cnt = contours[1], en cnt als var
     img_with_contour = cv.drawContours(img.cv_image, img.contours, -1, (255,0,255), 3)
     
-    # Compute the moment of contour:
-    M = cv.moments(img.contours[-1])
-    
-    # The center or centroid can be calculated as follows:
-    cX = int(M['m10'] / M['m00'])
-    cY = int(M['m01'] / M['m00'])
-    img_with_text = cv.putText(img_with_contour, "boutje", (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 255), 3)
+    for contour in range(len(img.contours)):
+        M = cv.moments(img.contours[contour])
+
+        cX = int(M['m10'] / M['m00'])
+        cY = int(M['m01'] / M['m00'])
+        
+        # TODO: Change to which object we need to indentify
+        text = "object"
+        
+        img_with_text = cv.putText(img_with_contour, text, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 255), 3)
     
     # -=-=- save image -=-=- #
     output_dir  = os.path.join(root_dir,'output_conventional', img.name)
