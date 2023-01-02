66e6e57f6735192e583dd141329aecec0b120b43
mart_laptop <mart.veldkamp@gmail.com>
2023-01-02 19:22:35 +0100

    added image class, setup in/out -put directories.

--- Conventional.py
+++ Conventional.py
@@ -1,40 +1,67 @@
 # -=-=-=- IMPORT LIBRARY'S -=-=-=- #
 import numpy as np
 import cv2 as cv
+import os
+import glob
 
-# -=-=-=- IMAGES UITLEZEN -=-=-=- #
-original_image = cv.imread('./test_images_2/foto_mat (1).jpg')
+# -=-=-=-=- IMAGE CLASS -=-=-=-=-
 
-hsv_image = cv.cvtColor(original_image, cv.COLOR_BGR2HSV)
+class image():
+    def __init__(self, cv_image, name):
+        self.cv_image = cv_image
+        self.name = name
     
-# -=-=-=- CONTOURS VINDEN -=-=-=- #
+    def set_color(self, color):
+        self.color = color
+    
+    def set_contours(self, contour):
+        self.contour = contour
+        
+            
+# -=-=-=- VARIABELEN -=-=-=-
+
+# Folder variabelen
+images      = []
+root_dir    = os.path.abspath("./")
+input_dir   = os.path.join(root_dir,'input_conventional','*.jpg')
+
 # Dit zijn de:         Hue, Saturation, Value
 lower_color = np.array([10,     20,      35])
 upper_color = np.array([30,     200,     180])
 
-# -=-=-=- IMAGE PRE-PROCESSING -=-=-=- #
-"""
-    1. haal alle aangegeven kleuren weg
-    2. inverteer afbeelding
-"""
-mask = cv.inRange(hsv_image, lower_color, upper_color)
-mask = cv.bitwise_not(mask)    
-
-# -=-=-=- ERODE / DILATE -=-=-=- #
 kernel = np.ones((4, 4), np.uint8)
 
-mask = cv.erode(mask, kernel, iterations=1)
-mask = cv.erode(mask, kernel, iterations=1)
+# -=-=-=- IMAGES UITLEZEN -=-=-=- #
 
-mask = cv.dilate(mask, kernel, iterations=1)
-mask = cv.dilate(mask, kernel, iterations=1)
-mask = cv.dilate(mask, kernel, iterations=1)
+for img in glob.glob(input_dir):
+    cv_img = cv.imread(img)
+    img = image(cv_img, os.path.basename(img))
+    images.append(img)
 
+# -=-=-=- IMAGES MASKEN -=-=-=- #
 
-# -=-=-=- CONTOURS VINDEN -=-=-=- #
-contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+for img in images:
 
-cv.drawContours(original_image, contours, -1, (255,0,255), 5)
+    # zet afbeelding in hue color space
+    hsv_image = cv.cvtColor(img.cv_image, cv.COLOR_BGR2HSV)
+    
+    # erode / dilate afbeelding voor betere kwaliteid masks
+    mask = cv.inRange(hsv_image, lower_color, upper_color)
+    mask = cv.bitwise_not(mask)
+    
+    mask = cv.erode(mask, kernel, iterations=1)
+    mask = cv.erode(mask, kernel, iterations=1)
 
-# -=-=-=- AFBEELDINGEN OPSLAAN -=-=-=- #
-cv.imwrite("./result_conventional/result1.jpg", original_image)
+    mask = cv.dilate(mask, kernel, iterations=1)
+    mask = cv.dilate(mask, kernel, iterations=1)
+    mask = cv.dilate(mask, kernel, iterations=1)
+    
+    # -=-=- contours vinden -=-=- #
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+    img.contour = cv.drawContours(img.cv_image, contours, -1, (255,0,255), 5)
+
+    # -=-=- afbeelding opslaan -=-=- #
+    output_dir  = os.path.join(root_dir,'output_conventional', img.name)
+    
+    cv.imwrite(output_dir, img.contour)
