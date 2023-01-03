b9a3bc52b8b745c87de6d5b0280a16896cb9dfd6
mart_laptop <mart.veldkamp@gmail.com>
2023-01-03 11:11:03 +0100

    added more info for item class and general added functions

--- Conventional.py
+++ Conventional.py
@@ -3,26 +3,35 @@ import numpy as np
 import cv2 as cv
 import os
 import glob
-print(cv.__version__)
-# -=-=-=-=- IMAGE CLASS -=-=-=-=- #
+
+# -=-=-=-=- CLASSES -=-=-=-=- #
 
 class image():
     
-    def __init__(self, cv_image, name):
-        self.contours   = []
-        self.cv_image   = cv_image
-        self.name       = name
+    def __init__(self, cv_image, doc_name: str):
+        self.contours: list = []
+        self.cv_image       = cv_image
+        self.name           = doc_name
     
     def add_contour(self, contour):
         self.contours.append(contour)
 
 class object():
-    def __init__(self):
-        self.kind_of_object = None
-        self.color          = None
-        self.position       = None
-        self.oriëntation    = None
-    
+    def __init__(self, outline, kind_of_object: str, position: tuple, oriëntation: float, color: str):
+        self.kind_of_object = kind_of_object
+        self.color          = color
+        self.position       = position
+        self.oriëntation    = oriëntation
+        self.outline        = outline
+
+# -=-=-=-=- DECLARE FUNCTIONS -=-=-=-=- #
+
+def find_object(contour, original_image) -> str:
+    return "nut"
+
+def find_color(contour, original_image) -> str:
+    return "color"
+
 # -=-=-=- VARIABLES -=-=-=- #
 
 # Folder variables
@@ -31,13 +40,22 @@ root_dir    = os.path.abspath("./")
 input_dir   = os.path.join(root_dir,'input_conventional','*.jpg')
 output_dir  = None # later defined 
 
-#                      Hue, Saturation, Value
-lower_color = np.array([10,     20,      35])
-upper_color = np.array([30,     200,     180])
+#             Hue, Saturation, Value
+lower_color = (10,     20,      35)
+upper_color = (30,     200,     180)
 
-contour_area = 900
+# how big the smallest area should be, the other ones get filtered away.
+filter_contour_area = 900
 
+# how big the kernel should be for the dilation / erosion.
 kernel = np.ones((4, 4), np.uint8)
+
+# pre-defined colors
+white  = np.array(["red",    (255,255,255)], dtype=object)
+black  = np.array(["black",  (0, 0, 0)],     dtype=object)
+pink   = np.array(["pink",   (300, 90 ,53)], dtype=object)
+metal  = np.array(["metal",  (0, 67 ,67)],   dtype=object)
+colors = (white, black, pink, metal)
 
 # -=-=-=- READ IMAGES  -=-=-=- #
 
@@ -64,34 +82,40 @@ for img in images:
     mask = cv.dilate(mask, kernel, iterations=1)
     mask = cv.dilate(mask, kernel, iterations=1)
     
-    # -=-=- find contours -=-=- #
+    # -=-=- FIND CONTOURS -=-=- #
     contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     
+    # -=- filter and add information to contours -=- #
     for contour in contours:
-        if (cv.contourArea(contour) > contour_area):
-            img.add_contour(contour)
-            contour = object()
-            
-    # print("found", len(img.contours), "contours in", img.name)
+        if (cv.contourArea(contour) > filter_contour_area):
+            # -=- find Moment and center point -=- #
+            M = cv.moments(contour)
+            cX = int(M['m10'] / M['m00'])
+            cY = int(M['m01'] / M['m00'])
 
-    # -=-=- find oriëntation -=-=- #
-    # TODO: Check this function
-    for contour in img.contours:
-        (x,y),(MA,ma),angle = cv.fitEllipse(contour)
-        object.oriëntation = angle
-        
-    # -=-=- find closest color -=-=- #
-    # TODO: make this function
-    
-    # -=-=- find kind of object -=-=- #
-    # TODO: Make this function
-    
+            # -=- find oriëntation -=- #
+            (x,y),(MA,ma),angle = cv.fitEllipse(contour)
+            
+            # -=- find closest color -=- #
+            color = find_color(contour, img)
+            
+            # -=- find kind of object -=- #
+            identified_item = find_object(contour, img)
+            
+            # -=- add to list -=- #
+            item = object(contour, identified_item, (cX, cY), angle, color)
+            img.add_contour(item.outline)
+            # img.contours = lijst aan contouren -> img.contours = lijst aan objects
+
+    print("found", len(img.contours), "contours in", img.name)
 
     # -=-=- draw contours and put text -=-=- #
     # to make a specific contour, use cnt = contours[1], and cnt as a var (instead of img.contours)
-    img_with_contour = cv.drawContours(img.cv_image, img.contours, -1, (255,0,255), 3)
+    
+    cv.drawContours(img.cv_image, img.contours, -1, (255,0,255), 3)
     
     for contour in range(len(img.contours)):
+        
         M = cv.moments(img.contours[contour])
 
         cX = int(M['m10'] / M['m00'])
@@ -100,8 +124,8 @@ for img in images:
         # TODO: Change to which object we need to indentify
         text = "object"
         
-        img_with_text = cv.putText(img_with_contour, text, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 255), 3)
+        cv.putText(img.cv_image, text, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 255), 3)
     
     # -=-=- save image -=-=- #
     output_dir  = os.path.join(root_dir,'output_conventional', img.name)
-    cv.imwrite(output_dir, img_with_text)
+    cv.imwrite(output_dir, img.cv_image)
